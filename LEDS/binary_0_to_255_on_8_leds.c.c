4fa855f3a429b5b926a4b92a8d2dc904640ddc01
V.Uday Sri Sai <160319564+Uday0606@users.noreply.github.com>
2026-02-10 12:12:20 +0530

    Add files via upload

---
+++ LEDS/binary_0_to_255_on_8_leds.c.c
@@ -0,0 +1,30 @@
+/*
+ Displays the binary equivalent of numbers from 0 to 255 on 8 LEDs.
+ P0.0–P0.3 : Active LOW LEDs
+ P0.4–P0.7 : Active HIGH LEDs
+*/
+
+#include<LPC213x.h>
+#include<stdint.h>
+
+void delay_ms(uint32_t dlyMs)
+{
+	uint32_t i;
+	for(i=0;i<dlyMs*12000;i++);
+}
+
+int main(void)
+{
+	uint32_t i,binary_num;
+	PINSEL0=0X00000000; //GPIO mode
+	IODIR0|=0XFF; //set PO.0 to p0.07 as output pin
+	
+	
+	for(i=0;i<256;i++)
+	{
+		binary_num=(i&0xF0)|(~i&0x0F);
+		IOPIN0=binary_num;
+		delay_ms(500);
+	}
+	while(1);
+}
